8f91158a338dd5b6ef0b8a32099161ba3b461063
Amir & Majid <dotadotnet@outlook.com>
2025-08-26 10:36:31 +0330

    ساخت پروژه سایت خبری با Next.js و Tailwind

--- components/buttons/SendButton.tsx
+++ components/buttons/SendButton.tsx
@@ -9,7 +9,7 @@ const SendButton: React.FC<SendButtonProps> = ({ isLoading = false }) => {
     return (
         <button
         type="submit"
-        className="group inline-flex items-center border border-green-300 dark:border-blue-600 px-4 py-2 rounded-md text-green-500 dark:text-blue-500 hover:bg-green-50 dark:hover:bg-gray-900 focus:outline-none focus:ring-2 focus:ring-green-500 dark:focus:ring-blue-500 transition-transform duration-300 transform group-hover:translate-x-1 group-focus:translate-x-1"
+        className="group  inline-flex items-center border border-green-300 dark:border-blue-600 px-4 py-2 rounded-md text-green-500 dark:text-blue-500 hover:bg-green-50 dark:hover:bg-gray-900 focus:outline-none focus:ring-2 focus:ring-green-500 dark:focus:ring-blue-500 transition-transform duration-300 transform group-hover:translate-x-1 group-focus:translate-x-1"
         disabled={isLoading}
       >
         {isLoading ? (
